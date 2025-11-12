ddf6e391087bb1817614680e2946392efdbc4ef5
AnastasiaSkachenko <skachenkoa@gmail.com>
2025-11-12 20:51:40 +0100

    Establish connection to database. CRUD functionality

---
+++ localEnv.js
@@ -0,0 +1,5 @@
+// It is not recommended to store sensitive information like service role keys in client-side code. 
+// However, for the purpose of this environment setup, we will define them here, as this is an educational project not intended for production use.
+
+export const supabaseUrl = "https://hbnquocvndpilfsrkefo.supabase.co"
+export const supabaseServiceRoleKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhibnF1b2N2bmRwaWxmc3JrZWZvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MjQxODIwOCwiZXhwIjoyMDc3OTk0MjA4fQ._0ymmw4NtxYvvr-AQAVXqM6TI42QfsDTr5F0bVZX9_4"
