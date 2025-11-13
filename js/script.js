7cd0db9903b002fb78a7b70fac09b3477e8c170d
mariamagdalenarios22@gmail.com <mariamagdalenarios22@gmail.com>
2025-11-13 14:45:49 +0100

    add current question and progress

--- js/script.js
+++ js/script.js
@@ -2,11 +2,22 @@ const totalQuestions = 10;
 let currentQuestion = 0;
 
 function updateProgressBar() {
+  
+  if (currentQuestion >= totalQuestions) {
+    return; 
+  }
+
   currentQuestion++;
   const progress = (currentQuestion / totalQuestions) * 100;
 
+  const text = document.getElementById("progress-text");
   const bar = document.getElementById("progress-bar");
-
+  
   bar.style.width = progress + "%";
   text.textContent = `Question ${currentQuestion} of ${totalQuestions}`;
+
+  console.log(`Current question: ${currentQuestion}`);
+  console.log(`Progress: ${progress}%`);
 }
+updateProgressBar()
+
