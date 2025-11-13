4487a29b7948cfec8b54fab3f3eb13d72e0d2c66
mariamagdalenarios22@gmail.com <mariamagdalenarios22@gmail.com>
2025-11-13 12:13:52 +0100

    add questions to json file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+const totalQuestions = 10;
+let currentQuestion = 0;
 
+function updateProgressBar() {
+  currentQuestion++;
+  const progress = (currentQuestion / totalQuestions) * 100;
+
+  const bar = document.getElementById("progress-bar");
+
+  bar.style.width = progress + "%";
+  text.textContent = `Question ${currentQuestion} of ${totalQuestions}`;
+}
