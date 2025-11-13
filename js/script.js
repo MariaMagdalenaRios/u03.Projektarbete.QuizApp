3f3fb4fe2610e3cf32ecea506c58b450cbadb58e
Hugo <hugo.oblak@outlook.com>
2025-11-13 13:41:59 +0100

    change script path and create randomizer function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+async function loadQuizData(quiz, category) {
+    const response = await fetch(`./data/${quiz}.json`);
+    const data = await response.json();
+    const questions = data[category];
+  
+    const randomQuestions = questions.sort(() => Math.random() - 0.5).slice(0, 10);
+  
+    console.log(randomQuestions);
+    return randomQuestions;
+  }
 
+  loadQuizData("music", "emoji")
+  
